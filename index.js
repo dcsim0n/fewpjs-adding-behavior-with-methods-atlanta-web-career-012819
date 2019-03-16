d472e233c085c0ca8669ff4edb1f5d1100704524
Dana Simmons <dana@simmons-audio.com>
2019-03-16 11:21:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+
+class Cat {
+    constructor(name, gender){
+        this.name = name
+        this.gender = gender
+    }
+
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name, gender){
+        this.name = name
+        this.gender = gender
+    }
+    speak(){
+       return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name,gender){
+        this.name = name
+        this.gender = gender
+    }
+    isFemale(){
+        if (this.gender == 'female'){
+            return true
+        }else{
+            return false
+        }
+    }
+    speak(){
+        if (this.isFemale() ){
+            return `${this.name} says squawk!`
+        }else{
+            return `It's me! ${this.name}, the parrot!`
+        }
+    }
+}
